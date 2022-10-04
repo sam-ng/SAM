c8beb145087e9d5d4224083e97390a811817618a
Samuel Ng <ngsamu4@gmail.com>
2022-10-04 16:19:58 -0400

    Added margins to project section

--- client/src/components/ProjectSection.jsx
+++ client/src/components/ProjectSection.jsx
@@ -5,10 +5,10 @@ import ProjectCard from './ProjectCard';
 const ProjectSection = ({ data }) => {
   return (
     <section className="mt-10 flex flex-col justify-center">
-      <h2 className="text-2xl font-Raleway font-semibold my-2">
+      <h2 className="mx-6 text-2xl font-Raleway font-semibold my-2">
         Featured Projects
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+      <div className="mx-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
         {data.allMdx.nodes.map(({ frontmatter, id }) => {
           const { name, description, link, image, tags } = frontmatter;
           return (
