1553c77b9f0a47c481fc46ad03a6794aefb2b37a
Samuel Ng <ngsamu4@gmail.com>
2022-10-06 20:02:53 -0400

    Added double major

--- client/src/components/AboutSnippet.jsx
+++ client/src/components/AboutSnippet.jsx
@@ -7,10 +7,10 @@ const AboutSnippet = () => {
         Hey there! I'm Samuel Ng.
       </h2>
       <p className="mt-5 text-xl text-center leading-loose font-Raleway">
-        I recently graduated with a BS in computer science from Stony Brook
-        University and am actively pursuing a career in software
-        development/engineering. I am super passionate about learning and
-        building new things.
+        I recently graduated with a BS in computer science and applied
+        mathematics & statistics from Stony Brook University and am actively
+        pursuing a career in software development/engineering. I am super
+        passionate about learning and building new things.
       </p>
     </section>
   );
