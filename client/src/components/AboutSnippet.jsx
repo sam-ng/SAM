99c8e2627a497fcc538ac0de1d222f386e60a484
sam-ng <ngsamu4@gmail.com>
2024-12-28 15:59:21 -0500

    Update bio on front page

--- client/src/components/AboutSnippet.jsx
+++ client/src/components/AboutSnippet.jsx
@@ -7,10 +7,10 @@ const AboutSnippet = () => {
         Hey there! I'm Samuel Ng.
       </h2>
       <p className="mt-5 text-xl text-center leading-loose font-Raleway">
-        I recently graduated with a BS in computer science and applied
-        mathematics & statistics from Stony Brook University and am actively
-        pursuing a career in software development/engineering. I am super
-        passionate about learning and building new things.
+        I'm currently a software developer for <a className="font-bold transition ease-in-out hover:text-sky-300" href='https://www.grantstreet.com/'>Grant Street Group</a>.
+      </p>
+      <p className="mt-5 text-xl text-center leading-loose font-Raleway">
+        I graduated with a BS in computer science and applied mathematics & statistics from Stony Brook University. Always eager to learn and build new things.
       </p>
     </section>
   );
